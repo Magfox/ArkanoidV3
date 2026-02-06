da317d1c18337548fa7af1c1717e6cea306386c3
Magfox <magfox2017@gmail.com>
2026-02-06 15:49:45 +1000

    9
    Исправно работает смена саб левела

--- Source/ArkanoidV3/Private/Core/ArkanoidGameState.cpp
+++ Source/ArkanoidV3/Private/Core/ArkanoidGameState.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e18c1fd793e7d36c160655017f079f53037172201304db84fd0efbadaeb3e327
-size 6211
+oid sha256:bee2e1649e9a424a81d23d5b41cdd09f448c4221768cf35e91dbf0db7e247ec3
+size 5585
